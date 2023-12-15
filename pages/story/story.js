98ad896bba3ff155a60aaec9d2860565f3d64df0
chenww <chenww@qq.com>
2023-12-15 16:14:00 +0800

    更新品牌故事的UI界面

--- pages/story/story.js
+++ pages/story/story.js
@@ -6,13 +6,13 @@ Page({
     swiperList_series: [
       {
         title: 'NUDO',
-        image: 'https://static.pomellato.cn/media/wysiwyg/6713a5e5b88e62ee613e18c9dd1eb3e.png',
-        text: '风靡20年Nudo美学设计，独特裸镶工艺，颗颗宝石萦绕生姿，\n 至尊匠心寻得多彩臻宝，诠释米兰斑斓色彩。\n',
+        image: '/images/beili (7).jpg',
+        text: '风靡2月Nudo美学设计，独特镶嵌工艺，颗颗宝石萦绕生姿，\n 至尊匠心寻得多彩臻宝，诠释中国斑斓色彩。\n',
       },
       {
         title: 'ICONCIA',
-        image: 'https://static.pomellato.cn/media/wysiwyg/6713a5e5b88e62ee613e18c9dd1eb3e.png',
-        text: '风靡20年Nudo美学设计，独特裸镶工艺，颗颗宝石萦绕生姿，\n 至尊匠心寻得多彩臻宝，诠释米兰斑斓色彩。\n',
+        image: '/images/beili (6).jpg',
+        text: 'lconica系列是Jewelry Smile微笑珠宝传承金匠工艺的最佳诠释,\n 中国匠心传承与绚丽璀璨宝石完美相契，凝练金璨摩登。\n',
       // 添加更多轮播项...
       }
     ],
@@ -20,12 +20,29 @@ Page({
     // CEO轮播图
     swiperList_ceo: [
       {
-        image: 'https://static.pomellato.cn/media/wysiwyg/6713a5e5b88e62ee613e18c9dd1eb3e.png',
-        text: '轮播项1',
+        image: '/images/ohhhhhh (1).jpg',
+        text: '',
+        text: '乔伊▪金（Joey King）\n',
       },
       {
-        image: 'https://static.pomellato.cn/media/wysiwyg/6713a5e5b88e62ee613e18c9dd1eb3e.png',
-        text: '轮播项2',
+        image:'/images/ohhhhhh (5).jpg',
+        text: '',
+        text: '宝拉▪艾拉努（Paola Egonu）\n',
+      },
+      {
+        image:'/images/ohhhhh.jpg',
+        text: '',
+        text: '纳西姆▪埃什齐\n',
+      },
+      {
+        image:'/images/ohhhhhh (7).jpg',
+        text: '',
+        text: '萨比那▪贝利\n',
+      },
+      {
+        image:'https://static.pomellato.cn/media/wysiwyg/6713a5e5b88e62ee613e18c9dd1eb3e.png',
+        text: '',
+        text: '简▪方达（Jane Fonda）',
       },
       // 添加更多轮播项...
     ],
@@ -34,12 +51,16 @@ Page({
     // 总监轮播图
     swiperList_commissioner: [
       {
-        image: 'https://static.pomellato.cn/media/wysiwyg/6713a5e5b88e62ee613e18c9dd1eb3e.png',
-        text: '轮播项1',
+        image: '/images/17 (1).jpg',
       },
       {
-        image: 'https://static.pomellato.cn/media/wysiwyg/6713a5e5b88e62ee613e18c9dd1eb3e.png',
-        text: '轮播项2',
+        image: '/images/17 (2).jpg',
+      },
+      {
+        image: '/images/17 (3).jpg',
+      },
+      {
+        image: '/images/17 (4).jpg',
       },
       // 添加更多轮播项...
     ],
@@ -47,12 +68,13 @@ Page({
     // 产品轮播图
     swiperList_product: [
       {
-        image: 'https://static.pomellato.cn/media/wysiwyg/6713a5e5b88e62ee613e18c9dd1eb3e.png',
-        text: '轮播项1',
+        image: '/images/17 (5).jpg',
       },
       {
-        image: 'https://static.pomellato.cn/media/wysiwyg/6713a5e5b88e62ee613e18c9dd1eb3e.png',
-        text: '轮播项2',
+        image: '/images/17 (6).jpg',
+      },
+      {
+        image: '/images/17 (7).jpg',
       },
       // 添加更多轮播项...
     ],
